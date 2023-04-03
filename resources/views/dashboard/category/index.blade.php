5061c0166a883c0f030f207d7005a05af2ace680
Alexandra Enciso <alexandra.enciso@iudigital.edu.co>
2023-04-02 22:14:55 -0500

    segundo guardado proyecto

--- resources/views/dashboard/category/index.blade.php
+++ resources/views/dashboard/category/index.blade.php
@@ -1,9 +1,11 @@
-@extends('dashboard.master')
-@section('titulo', 'Poster')
-@section('contenido')
-    <main>
-        <div class="container py-4">
-            <h2>Categorias</h2>
+<x-app-layout>
+    <x-slot name="header">
+        <h1 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Catetegorías') }}
+        </h1>
+    </x-slot>
+<div class="container py-4">
+            
             <a href="{{ url('dashboard/category/create') }}" class="btn btn-primary btn-sm">Nueva categoria</a>
 
             <table class="table table-dark table-striped">
@@ -41,4 +43,18 @@
             </table>
         </div>
     </main>
-@endsection
+
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
+    <main>
+        
+
